5f27d604a36357ce59559f0c59bd6d93e561b85a
Silvio Paganini <s2paganini@gmail.com>
2016-05-16 23:27:15 +0100

    hover details

--- src/index.js
+++ src/index.js
@@ -17,8 +17,32 @@ function stopVideo(e)
   video.pause();
 }
 
-let buttons = document.querySelectorAll('a.container-project');
+let buttons = document.querySelectorAll('.container-project');
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('mouseover', playVideo);
   buttons[i].addEventListener('mouseleave', stopVideo);
+}
+
+let subButtons = document.querySelectorAll('.container-project > .submenu > button');
+for(var i = 0; i<subButtons.length; i++)
+{
+  subButtons[i].addEventListener('click', function(e){
+    e.preventDefault();
+    window.open(e.target.dataset.href)
+  });
+}
+
+let submenus = document.querySelectorAll('.container-project > .submenu');
+console.log(submenus);
+for(var i = 0; i<submenus.length; i++)
+{
+  let a = submenus[i]
+  a.parentNode.addEventListener('click', function(e){
+    var display = a.currentStyle ? a.currentStyle.display : getComputedStyle(a, null).display;
+
+    if(display != 'none') {
+      e.preventDefault();
+    }
+    
+  }.bind(this));
 }
